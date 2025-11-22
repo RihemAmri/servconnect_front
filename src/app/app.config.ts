d1bd6b726fe1a2a97773ba7bd2e92e3f9de19764
nadabentaher2003@gmail.com <nadabentaher2003@gmail.com>
2025-11-22 17:23:34 +0100

    ajouter inscri map

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideLottieOptions } from 'ngx-lottie';
 import player from 'lottie-web';
 
@@ -12,6 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
     provideAnimations(), // ✅ il faut les parenthèses ici
     provideLottieOptions({
